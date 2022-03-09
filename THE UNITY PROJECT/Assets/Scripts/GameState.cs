036e0cc7fbc2a66d82a365cd294fc753c8da0989
Francois <francoiskrugermail@gmail.com>
2022-03-09 07:44:36 +0200

    Added place tower functionality

--- THE UNITY PROJECT/Assets/Scripts/GameState.cs
+++ THE UNITY PROJECT/Assets/Scripts/GameState.cs
@@ -6,6 +6,8 @@ public class GameState : MonoBehaviour
 {
     public GameObject hoverTower;
 
+    private Vector3 mouseWorldPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,33 @@ public class GameState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveMouseTower();
+        PlaceTower();
+    }
+
+    // The logic that hover's the placeholder tower on the user's mouse position
+    void MoveMouseTower()
+    {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = Camera.main.nearClipPlane; // We do this otherwise the Z value can be a minus value, therefore hiding the object
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
         hoverTower.transform.position = mouseWorldPosition;
+    }
+
+    // The logic to place a tower onto the field
+    void PlaceTower()
+    {
+        if (Input.GetMouseButtonDown(0)) // Mouse left click
+        {
+            // Spawn the tower
+            GameObject tempObject = Instantiate(hoverTower, mouseWorldPosition, Quaternion.identity);
+            
+            // Make it's opcaity/alpha value solid
+            SpriteRenderer renderer = tempObject.GetComponent<SpriteRenderer>();
+            Color newColor = renderer.material.color;
+            Debug.Log(newColor);
+            newColor.a = 1;
+            renderer.material.color = newColor;
+        }
     }
 }
